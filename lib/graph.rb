e1af3cf5318f26d76dfea75fdc5ce50aaae79a17
AlisonZ <alisonkzerbe@gmail.com>
2018-01-31 20:39:05 -0800

    moves files to lib folder and creates testing spec folder

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,5 +1,5 @@
-require './Edge.rb'
-require './Node.rb'
+require './lib/Edge.rb'
+require './lib/Node.rb'
 
 class Graph
     attr_accessor :routeHash, :graphList
@@ -164,42 +164,15 @@ class Graph
         puts distance
     end
 end
-
-# TODO: clean this up. working now, but only for this problem set. should be nice and recursive
-def make_graph()
-    g = Graph.new
-    list = create_graph_list('./input.txt')
-
-    list.each do |route|
-        if g.routeHash[route[0]]
-            if g.routeHash[route[0]].next === false
-                g.routeHash[route[0]].add_next(Edge.new(route[0], route[1], route[2].to_i))
-            else
-                # this works for this specific input, but wouldn't work with another next loop
-                # make this recursive and work for real, not hacky
-                # until .next != false keep putting node.next into the checker and then call add_next on it
-                g.routeHash[route[0]].next.add_next(Edge.new(route[0], route[1], route[2].to_i))
-            end
-        else
-            g.routeHash[route[0]] = Edge.new(route[0], route[1], route[2].to_i)
-        end
-    end
-
-    return g
-
-end
-
-# g = make_graph()
-
-# # MANUALLY ADDS THE NODES AND EDGES
+#
+# # # MANUALLY ADDS THE NODES AND EDGES
 g = Graph.new
-g.graphList = g.createGraphList("input.txt")
+g.graphList = g.createGraphList("./lib/input.txt")
 A = Node.new("A")
 B = Node.new("B")
 C = Node.new("C")
 D = Node.new("D")
 E = Node.new("E")
-
 
 g.routeHash[A] = Edge.new(A, B, 5)
 g.routeHash[A].add_next(Edge.new(A, D, 5))
@@ -210,25 +183,25 @@ g.routeHash[C].add_next(Edge.new(C, E, 2))
 g.routeHash[D] = Edge.new(D, C, 8)
 g.routeHash[D].add_next(Edge.new(D, E, 6))
 g.routeHash[E] = Edge.new(E, B, 3)
-
-
-#QUESTION 1: EXACT ROUTE
-# g.exactRoute("A", "B", "C")
-#QUESTION 2: EXACT ROUTE
-# g.exactRoute("A", "D")
-#QUESTION 3: EXACT ROUTE
-# g.exactRoute("A", "D", "C")
-#QUESTION 4: EXACT ROUTE
-# g.exactRoute("A", "E", "B", "C", "D")
-#QUESTION 5: EXACT ROUTE
-# g.exactRoute("A", "E", "D")
-# QUESTION 6: # OF TRIPS STARTING AT X, ENDING AT Y WITH MAX STOPS
+#
+#
+# #QUESTION 1: EXACT ROUTE
+# # g.exactRoute("A", "B", "C")
+# #QUESTION 2: EXACT ROUTE
+# # g.exactRoute("A", "D")
+# #QUESTION 3: EXACT ROUTE
+# # g.exactRoute("A", "D", "C")
+# #QUESTION 4: EXACT ROUTE
+# # g.exactRoute("A", "E", "B", "C", "D")
+# #QUESTION 5: EXACT ROUTE
+# # g.exactRoute("A", "E", "D")
+# # QUESTION 6: # OF TRIPS STARTING AT X, ENDING AT Y WITH MAX STOPS
 g.numStops("C", "C", 3)
-# QUESTION 7: # OF TRIPS STARTING AT X, ENDING AT Y WITH EXACT STOPS
-# g.exactStops("A", "C", 4)
-# QUESTION 8: SHORTEST DISTANCE BETWEEN X & Y
-# g.shortestDistance("A", "C")
-# QUESTION 9: SHORTEST DISTANCE BETWEEN X & Y
-# g.shortestDistance("B", "B")
-#QUESTION 10: ROUTES BETWEEN X AND Y WITHIN MAX DISTANCE
-# g.routesWithin("C", "C", 30)
+# # QUESTION 7: # OF TRIPS STARTING AT X, ENDING AT Y WITH EXACT STOPS
+g.exactStops("A", "C", 4)
+# # QUESTION 8: SHORTEST DISTANCE BETWEEN X & Y
+g.shortestDistance("A", "C")
+# # QUESTION 9: SHORTEST DISTANCE BETWEEN X & Y
+g.shortestDistance("B", "B")
+# #QUESTION 10: ROUTES BETWEEN X AND Y WITHIN MAX DISTANCE
+# # g.routesWithin("C", "C", 30)
