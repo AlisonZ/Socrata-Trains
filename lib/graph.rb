2566b5976d271ab8a28808c38277dff639e8a8b6
AlisonZ <alisonkzerbe@gmail.com>
2018-01-31 23:07:27 -0800

    adds tests

--- lib/graph.rb
+++ lib/graph.rb
@@ -26,17 +26,40 @@ class Graph
         return start
     end
 
+    def exactRoute(*args)
+        stations = args
+        routes = Hash.new
+
+        self.graphList.each do |route|
+            routes[route[0,2]] = route[2]
+        end
+
+        i = 0
+        distance = 0
+        while i < stations.length-1
+            route = stations[i].upcase+stations[i+1].upcase
+            if routes[route]
+                distance += routes[route].to_i
+                i+=1
+            else
+                return "NO SUCH ROUTE"
+            end
+        end
+
+        return distance
+    end
+
     def numStops(start, final, maxStops)
-        puts findRoutes(start.upcase, final.upcase, 0, maxStops)
+        return findRoutes(start.upcase, final.upcase, 0, maxStops)
     end
 
     def findRoutes(start, final, depth, maxStops)
         start = self.find_node(start)
         destination = self.find_node(final)
-        routes = 0
-        depth +=1
         # TODO:ideally this would check if a Node, but hit a Ruby snag
         if start.class != String && destination.class != String
+            depth +=1
+            routes = 0
             if depth > maxStops
                 return routes
             else
@@ -61,13 +84,13 @@ class Graph
             start.visited = false
             return routes
         else
-            puts "NO SUCH ROUTE"
+            return "NO SUCH ROUTE"
         end
     end
 
 
     def exactStops(start, final, exactStops)
-        puts exactStopsRoutes(start.upcase, final.upcase, 0, exactStops)
+        return exactStopsRoutes(start.upcase, final.upcase, 0, exactStops)
     end
 
     def exactStopsRoutes(start, final, stops, exactStops)
@@ -101,12 +124,12 @@ class Graph
             start.visited = false
             return routes
         else
-            puts "NO SUCH ROUTE"
+            return "NO SUCH ROUTE"
         end
     end
 
     def shortestDistance(start, finish)
-        puts findShortestDistance(start.upcase, finish.upcase, 0, 0)
+        return findShortestDistance(start.upcase, finish.upcase, 0, 0)
     end
 
     def findShortestDistance(start, finish, weight=0, shortestDistance=0)
@@ -137,31 +160,8 @@ class Graph
             start.visited = false
             return shortestDistance
         else
-            puts "NO SUCH ROUTE"
+            return "NO SUCH ROUTE"
         end
-    end
-
-    def exactRoute(*args)
-        stations = args
-        routes = Hash.new
-
-        self.graphList.each do |route|
-            routes[route[0,2]] = route[2]
-        end
-
-        i = 0
-        distance = 0
-        while i < stations.length-1
-            route = stations[i]+stations[i+1]
-            if routes[route]
-                distance += routes[route].to_i
-                i+=1
-            else
-                puts "NO SUCH ROUTE"
-                return
-            end
-        end
-        puts distance
     end
 end
 #
@@ -186,22 +186,22 @@ g.routeHash[E] = Edge.new(E, B, 3)
 #
 #
 # #QUESTION 1: EXACT ROUTE
-# # g.exactRoute("A", "B", "C")
+ # puts g.exactRoute("A", "B", "C")
 # #QUESTION 2: EXACT ROUTE
-# # g.exactRoute("A", "D")
+ # puts g.exactRoute("A", "D")
 # #QUESTION 3: EXACT ROUTE
-# # g.exactRoute("A", "D", "C")
+#  puts g.exactRoute("A", "D", "C")
 # #QUESTION 4: EXACT ROUTE
-# # g.exactRoute("A", "E", "B", "C", "D")
+#  puts g.exactRoute("A", "E", "B", "C", "D")
 # #QUESTION 5: EXACT ROUTE
-# # g.exactRoute("A", "E", "D")
+# puts g.exactRoute("A", "E", "D")
 # # QUESTION 6: # OF TRIPS STARTING AT X, ENDING AT Y WITH MAX STOPS
-g.numStops("C", "C", 3)
+ # puts g.numStops("C", "C", 3)
 # # QUESTION 7: # OF TRIPS STARTING AT X, ENDING AT Y WITH EXACT STOPS
-g.exactStops("A", "C", 4)
+ # puts g.exactStops("A", "C", 4)
 # # QUESTION 8: SHORTEST DISTANCE BETWEEN X & Y
-g.shortestDistance("A", "C")
+# puts g.shortestDistance("A", "C")
 # # QUESTION 9: SHORTEST DISTANCE BETWEEN X & Y
-g.shortestDistance("B", "B")
+#  puts g.shortestDistance("B", "B")
 # #QUESTION 10: ROUTES BETWEEN X AND Y WITHIN MAX DISTANCE
-# # g.routesWithin("C", "C", 30)
+# # puts g.routesWithin("C", "C", 30)
--- specs/graph_spec.rb
+++ specs/graph_spec.rb
@@ -53,13 +53,116 @@ end
 
 describe "Graph class" do
     before do
-        # create nodes
-        # create edges
-        # create graph from nodes & edges
-        # create graphList for the exactRoute function
+        @my_graph = Graph.new
+        @A = Node.new("A")
+        @B = Node.new("B")
+        @C = Node.new("C")
+        @D = Node.new("D")
+        @E = Node.new("E")
+
+
+        @my_graph.routeHash[A] = Edge.new(A, B, 5)
+        @my_graph.routeHash[A].add_next(Edge.new(A, D, 5))
+        @my_graph.routeHash[A].next.add_next(Edge.new(A, E, 7))
+        @my_graph.routeHash[B] = Edge.new(B, C, 4)
+        @my_graph.routeHash[C] = Edge.new(C, D, 8)
+        @my_graph.routeHash[C].add_next(Edge.new(C, E, 2))
+        @my_graph.routeHash[D] = Edge.new(D, C, 8)
+        @my_graph.routeHash[D].add_next(Edge.new(D, E, 6))
+        @my_graph.routeHash[E] = Edge.new(E, B, 3)
+
+        @my_graph.graphList = @my_graph.createGraphList("./lib/input.txt")
+
     end
+
     it "creates a graph" do
-        my_graph = Graph.new
-        my_graph.class.must_equal Graph
+        @my_graph.must_be_instance_of Graph
     end
+    describe "exactRoute" do
+        it "takes an exact route and finds the distance" do
+            output1 = @my_graph.exactRoute("A", "B", "C")
+            output1.must_equal 9
+
+            output2 = @my_graph.exactRoute("A", "D")
+            output2.must_equal 5
+
+            output3 = @my_graph.exactRoute("A", "D", "C")
+            output3.must_equal 13
+
+            output4 = @my_graph.exactRoute("A", "E", "B", "C", "D")
+            output4.must_equal 22
+        end
+        it "prints out NO SUCH ROUTE if there is no route that matches input" do
+            output = @my_graph.exactRoute("A", "E", "D")
+            output.must_equal "NO SUCH ROUTE"
+
+        end
+        it "finds the exact route of lowercase input" do
+            output = @my_graph.exactRoute("a", "b", "c")
+            output.must_equal 9
+        end
+
+    end
+
+    describe "numStops" do
+        it "takes start and finish stops and finds number of routes with max stops" do
+            routes = @my_graph.numStops("C", "C", 3)
+            routes.must_equal 2
+        end
+        it "prints out NO SUCH ROUTES if invalid input" do
+            routes = @my_graph.numStops("E", "Z", 1)
+            routes.must_equal "NO SUCH ROUTE"
+
+        end
+        it "finds routes with lowercase input" do
+            routes = @my_graph.numStops("c", "c", 3)
+            routes.must_equal 2
+        end
+    end
+
+    describe "exactStops" do
+        it "takes start and finish stops and finds number of routes with exact number of stops" do
+            routes = @my_graph.exactStops("A", "C", 4)
+            routes.must_equal 3
+        end
+        it "finds routes with lowercase input" do
+            routes = @my_graph.exactStops("a", "c", 4)
+            routes.must_equal 3
+        end
+        it "returns NO SUCH ROUTES with invalid input" do
+            routes = @my_graph.exactStops("A", "Z", 4)
+            routes.must_equal "NO SUCH ROUTE"
+        end
+    end
+
+    describe "shortestDistance" do
+        it "takes a start and finish and find shortest route" do
+            distance = @my_graph.shortestDistance("A", "C")
+            distance.must_equal 9
+        end
+        it "finds distance with lowercase input" do
+            distance = @my_graph.shortestDistance("a", "C")
+            distance.must_equal 9
+        end
+        it "returns NO SUCH INPUT with invalid input" do
+            distance = @my_graph.shortestDistance("A","Z")
+            distance.must_equal "NO SUCH ROUTE"
+        end
+    end
+
+    xdescribe "routesWithin" do
+        it "takes a start and stop with max distance and returns number of routes" do
+            routes = @my_graph.routesWithin("C", "C", 30)
+            routes.must_equal 7
+        end
+        it "finds routes with lowercase input" do
+            routes = @my_graph.routesWithin("c", "c", 30)
+            routes.must_equal 7
+        end
+        it "returns NO SUCH ROUTE with invalid input" do
+            routes = @my_graph.routesWithin("A", "Z", 16)
+            routes.must_equal "NO SUCH ROUTE"
+        end
+    end
+
 end
