a17552c5fb62c7331bbd434be60c290f32741a71
AlisonZ <alisonkzerbe@gmail.com>
2018-01-31 19:53:45 -0800

    moves node and edge classes to own files

---
+++ Edge.rb
@@ -0,0 +1,17 @@
+class Edge
+    attr_accessor :origin, :destination, :weight, :next
+
+    def initialize(origin, destination, weight)
+        @origin = origin
+        @destination = destination
+        @weight = weight
+        @next = false
+    end
+
+    # TODO: remove this function if it does not end up being used recursively
+    # can do the same thing now as just updating the .next feature of the edge currently on
+    def add_next(edge)
+        self.next = edge
+        return edge
+    end
+end
---
+++ Node.rb
@@ -0,0 +1,8 @@
+class Node
+    attr_accessor :name, :visited
+
+    def initialize(name)
+        @name = name
+        @visited = false
+    end
+end
